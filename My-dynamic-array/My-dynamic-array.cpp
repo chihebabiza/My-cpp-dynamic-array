473b5efa70f48055dd84e9b6bc569d9db1ddb5f6
chiheb <chihebabiza9@gmail.com>
2025-06-17 12:09:02 +0100

    dynamic array is dont

--- My-dynamic-array/My-dynamic-array.cpp
+++ My-dynamic-array/My-dynamic-array.cpp
@@ -1,20 +1,29 @@
-// My-dynamic-array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "clsDynamicArray.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    clsDynamicArray <int> MyDynamicArray(10);
+
+    MyDynamicArray.SetItem(0, 10);
+    MyDynamicArray.SetItem(1, 20);
+    MyDynamicArray.SetItem(2, 30);
+    MyDynamicArray.SetItem(3, 40);
+    MyDynamicArray.SetItem(4, 50);
+
+    cout << "\nIs Empty?  " << MyDynamicArray.IsEmpty();
+    cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    cout << "\nArray Items: \n";
+
+    MyDynamicArray.PrintList();
+
+
+
+
+
+    system("pause>0");
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
